3abfc9dd1a7e8d4d07008fd68b6644e5b3708309
henriquefac <henrique.fdut@gmail.com>
2023-11-21 14:37:54 -0300

    Algoritmo de ordenação - força bruta

---
+++ Ordenacao/BruteForce.py
@@ -0,0 +1,14 @@
+#Gerar vetor
+from vetorAleatorio import vetorAleatorio
+
+vetor = vetorAleatorio()
+
+print(vetor)
+aux = 0
+for i in range(len(vetor)):
+    for j in range(len(vetor)):
+        if vetor[i] > vetor[j]:
+            aux = vetor[i]
+            vetor[i] = vetor[j]
+            vetor[j] = aux
+print(vetor)
